6c1b9e2b1cb5c9aece65864b9d136259e5121457
jayckaiser <jayckaiser@gmail.com>
2025-09-19 23:27:24 -0500

    Refactor bar and pie to allow arguments to be generically passed from YAML.

--- project_den/figure.py
+++ project_den/figure.py
@@ -3,13 +3,10 @@ import plotly.graph_objects as go
 
 from project_den import util
 
-from typing import Optional
+from typing import List, Optional
 
 
 class Figure:
-    # TODO: Make this dynamic
-    PALETTE = px.colors.diverging.Fall
-
     def __new__(cls, *args, type: str, **kwargs):
         if type == "bar":
             return object.__new__(BarFigure)
@@ -19,107 +16,60 @@ class Figure:
             return object.__new__(TableFigure)
         raise NotImplementedError(f"! Figure type `{type}` is undefined!")
 
-    def __init__(self, sql: str, name: str, title: Optional[str] = None, **kwargs):
+    def __init__(self,
+        name: str, sql: str,
+        figures: List[dict],
+        layout: Optional[dict] = None,
+        traces: Optional[dict] = None,
+        **kwargs  # Stores `type` to avoid collision with built-in
+    ):
         self.name = name
-        self.title = title
-
         self.sql = sql
         self.data = util.sql(sql)
-        self.figure = None  # Initialize empty figure
+        self.figure = go.Figure()  # Traces added to empty figure
+
+        # Figure construction
+        for fig_kwargs in figures:
+            sub_figure = self.figure_callable(self.data, **fig_kwargs)
+            for trace in sub_figure.data:
+                self.figure.add_trace(trace)
+        
+        # Optional formatting
+        if layout:
+            self.figure.update_layout(**layout)
+        if traces:
+            self.figure.update_traces(**traces)
 
     def show(self):
         self.figure.show()
 
-    def parse_optional_cols(self, column: str):
+    @staticmethod
+    def figure_callable(*args, **kwargs):
+        """
+        Defined in child classes. 
 
-        # Return all column names if no data specified
-        if column == "*":
-            return list(self.data.columns)
-        
-        # Return all values if all columns are specified
-        if column == "$*":
-            return self.data
-
-        if column.startswith("$"):
-            return self.data[column.lstrip("$")]
-        
-        return column
+        TODO: Ideally, I'd just use `figure_callable = px.bar` or similar, but it is not static.
+              We get errors because `self` is passed into `px.METHOD`.
+        """
+        raise NotImplementedError
 
 
 class BarFigure(Figure):
-
-    def __init__(self, *args, x, y, labels, color, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        if isinstance(x, str):
-            x = [x]
-        if isinstance(y, str):
-            y = [y]
-        if isinstance(labels, str):
-            labels = [labels]
-        if isinstance(color, str):
-            color = [color]
-        
-        for idx, (
-            fig_x, fig_y, fig_labels, fig_color
-        ) in enumerate(zip(
-            x, y, labels, color
-        )):
-            fig = px.bar(
-                self.data,
-                title=self.title,
-                color_discrete_sequence=self.PALETTE,
-
-                x=fig_x,
-                y=fig_y,
-                color=fig_color,
-                text=fig_labels,
-            )
-
-            # TODO: Overlaying figures should be genericized
-            if idx == 0:
-                self.figure = go.Figure(fig)
-            else:
-                for dd in fig.data:
-                    self.figure.add_trace(dd)
-
-        self.figure.update_layout(
-            yaxis=dict(tickformat="d"),
-            showlegend=False,
-            xaxis_title=None,
-            yaxis_title=None,
-            barmode="overlay",
-        )
-
-        self.figure.update_traces(
-            textfont_size=16,
-        )
+    """
+    https://plotly.com/python-api-reference/generated/plotly.express.bar
+    """
+    @staticmethod
+    def figure_callable(*args, **kwargs):
+        return px.bar(*args, **kwargs)
 
 
 class PieFigure(Figure):
-    def __init__(self, *args, values, labels, color, color_mapping, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # Convert to lists to allow easier column-parsing
-        data = px.pie(
-            self.data,
-            title=self.title,
-            values=values,
-            names=labels,
-            color=color,
-            color_discrete_map=color_mapping,
-            hole=0.5
-        )
-
-        self.figure = go.Figure(data=data)
-
-        self.figure.update_layout(
-            showlegend=False,
-        )
-
-        self.figure.update_traces(
-            hoverinfo='label+percent', textinfo='label+percent+value', textfont_size=16,
-        )
+    """
+    https://plotly.com/python-api-reference/generated/plotly.express.pie
+    """
+    @staticmethod
+    def figure_callable(*args, **kwargs):
+        return px.pie(*args, **kwargs)
 
 
 # TODO: Finish table
