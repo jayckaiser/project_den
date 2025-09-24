40e48a0e06e04d176dd09cdafdd6fd8e939daa5a
jayckaiser <jayckaiser@gmail.com>
2025-09-23 22:01:28 -0500

    Remove accidental double-defined method.

--- project_den/util.py
+++ project_den/util.py
@@ -44,8 +44,6 @@ def time_filter(years: List[int], months: List[int]) -> str:
     school_year IN ('{year_str}')
     AND MONTH(visit_date) IN ('{month_str}')
     """
-    import calendar
-
     # Force to strings for easy-joining.
     years = list(map(str, years))
     months = list(map(str, months))
@@ -72,47 +70,3 @@ def load_yaml(path: str, **kwargs) -> dict:
         configs = template.render(kwargs)
 
     return yaml.safe_load(configs)
-
-def time_filter(years: List[int], months: List[int]):
-    months_repr = None  # Initialize optionals as Nones
-    years_repr = None
-
-    if months:
-        # Months repeat at 12
-        months_modulo_list = sorted((mm + 12) if mm < 6 else mm for mm in months)
-
-        if _is_consecutive(months_modulo_list):
-            months_repr = "{}-{}".format(
-            calendar.month_name[_from_mod12(min(months_modulo_list))],
-            calendar.month_name[_from_mod12(max(months_modulo_list))]
-            )
-        else:
-            months_repr = ", ".join(calendar.month_name[_from_mod12(mm)] for mm in months_modulo_list)
-
-    if years:
-        years_list = sorted(years)
-
-        if _is_consecutive(years_list):
-            years_repr = "{}-{}".format(
-            min(years_list),
-            max(years_list)
-            )
-        else:
-            years_repr = ", ".join(years_list)
-
-    time_filter_string = " ".join(filter(None, (months_repr, years_repr)))
-    return f"({time_filter_string})"
-
-def _is_consecutive(elements: list) -> bool:
-    elements = list(map(int, elements))
-    if len(elements) <= 1:
-        return False
-
-    sorted_list = sorted(elements)
-    return all(sorted_list[i] == sorted_list[i-1] + 1 for i in range(1, len(sorted_list)))
-
-def _from_mod12(item: int) -> int:
-    if remainder := item % 12:
-        return remainder
-    else:
-        return 12
